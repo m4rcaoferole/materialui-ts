565adbf00251e137203e8a099eea0a011561809d
m4rcaoferole <marcao.adsistema@gmail.com>
2022-04-14 05:57:42 -0300

    feat: adicionado sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { AppRoutes } from "./routes";
+import { Sidebar } from "./shared/components/sidebar/Sidebar";
 import { AppThemeProvider } from "./shared/contexts";
 
 export const App = () => {
   return (
     <AppThemeProvider>
       <BrowserRouter>
-        <AppRoutes />
+        <Sidebar>
+          <AppRoutes />
+        </Sidebar>
       </BrowserRouter>
     </AppThemeProvider>
   );
---
+++ src/shared/components/sidebar/Sidebar.tsx
@@ -0,0 +1,64 @@
+import {
+  Avatar,
+  Divider,
+  Drawer,
+  Icon,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+} from "@mui/material";
+import { deepOrange } from "@mui/material/colors";
+import { Box } from "@mui/system";
+
+export const Sidebar: React.FC = ({ children }) => {
+  const theme = useTheme();
+
+  return (
+    <>
+      <Drawer variant="permanent">
+        <Box
+          width={theme.spacing(28)}
+          height="100%"
+          display="flex"
+          flexDirection="column"
+        >
+          <Box
+            width="100%"
+            height={theme.spacing(20)}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Avatar
+              sx={{
+                height: theme.spacing(12),
+                width: theme.spacing(12),
+                bgcolor: deepOrange[500],
+              }}
+              alt="Remy Sharp"
+              src="/broken-image.jpg"
+            >
+              M
+            </Avatar>
+          </Box>
+          <Divider />
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Página Inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
+      <Box height="100vh" marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
+};

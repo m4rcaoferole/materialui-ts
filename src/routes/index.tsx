3544c4624f0f97b86a56b6419cced9afe86426d2
Marcos Vinicius <marcao.vfo@gmail.com>
2022-05-01 10:42:40 -0400

    feat: adicionado responsividade no sidebar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,27 @@
-import { Button } from '@mui/material'
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { useAppThemeContext } from '../shared/contexts';
+import { Button } from "@mui/material";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import { useDrawerContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
-    const { toggleTheme } = useAppThemeContext();
+  const { toggleDrawerOpen } = useDrawerContext();
 
-    return (
-        <Routes>
-            <Route path="/pagina-inicial" element={<Button variant='contained'color='primary' onClick={toggleTheme}>Toggle Theme</Button>} />
+  return (
+    <Routes>
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={toggleDrawerOpen}
+          >
+            Toggle Drawer
+          </Button>
+        }
+      />
 
-            <Route path="*" element={<Navigate to="/pagina-inicial" />} />
-        </Routes>
-    )
-}
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+};

dada9cad4db6aef9e2ba64df4e6f25850efe08c2
Marcos Vinicius <marcao.vfo@gmail.com>
2022-11-20 20:53:45 -0400

    feat: config variavels envirinment

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -1,1 +1,18 @@
-export { };
+export const Environment = {
+  /**
+   * Define a quantidade de linhas a ser carregada por padrão nos listagens
+   */
+  LIMITE_DE_LINHAS: 10,
+  /**
+   * Placeholder exibido nas inputs
+   */
+  INPUT_DE_BUSCA: 'Pesquisar...',
+  /**
+   * Texto exibido quando nenhum registro é encontrado em uma listagem.
+   */
+  LISTAGEM_VAZIA: 'Nenhum registro encontrado',
+  /**
+   * Url base de consulta dos dados da aplicação
+   */
+  URL_BASE: 'http://localhost:3333',
+};

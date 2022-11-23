6e4ff162283b530c84d7c631db3a03d9a4339979
Marcos Vinicius <marcao.vfo@gmail.com>
2022-11-23 13:01:35 -0400

    feat: consulta delete e edit - handleevent click delete

--- src/pages/people/DetailPeople.tsx
+++ src/pages/people/DetailPeople.tsx
@@ -1,23 +1,59 @@
+import { LinearProgress } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+
 import { ToolbarDetail } from '../../shared/components';
 import { LayoutBasePage } from '../../shared/layouts';
+import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
 
 export const DetailPeople = () => {
   const { id = '' } = useParams<'id'>();
   const navigate = useNavigate();
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [nome, setNome] = useState('');
+
+  useEffect(() => {
+    if (id !== 'nova') {
+      setIsLoading(true);
+
+      PessoasService.getById(Number(id))
+        .then((result) => {
+          setIsLoading(false);
+
+          if (result instanceof Error) {
+            alert(result.message);
+            navigate('/pessoas');
+          } else {
+            setNome(result.nomeCompleto);
+            console.log(result);
+          }
+        });
+    }
+  }, [id]);
+
   const handleSave = () => {
     console.log('save');
   };
 
-  const handleDelete = () => {
-    console.log('delete');  
+  const handleDelete = (id: number) => {
+    if (confirm('Realmente deseja apagar?')) {
+      PessoasService.deleteById(id)
+        .then(result => {
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            alert('Registro apagado com sucesso!');
+            navigate('/pessoas');
+          }
+        }); 
+    }
   };
 
   return (
     <LayoutBasePage
-      title="Detalhe de pessoa"
+      title={id === 'nova' ? 'Nova pessoa' : nome}
       toolbar={
         <ToolbarDetail
           textNewButton='Nova'
@@ -27,12 +63,16 @@ export const DetailPeople = () => {
 
           clickSaveButton={handleSave}
           clickSaveAndCloseButton={handleSave}
-          clickDeleteButton={handleDelete}
+          clickDeleteButton={() => handleDelete(Number(id))}
           clickBackButton={() => navigate('/pessoas')}
           clickNewButton={() => navigate('/pessoas/detalhe/nova')}
         />
       }
     >
+      { isLoading && (
+        <LinearProgress variant="indeterminate" />
+      )}
+
       <p> Detalhe de Pessoa {id}</p>
     </LayoutBasePage>
   );

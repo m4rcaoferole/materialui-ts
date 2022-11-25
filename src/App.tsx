b241ae550151eb6b74c661b4bb9b14758459dd69
Marcos Vinicius <marcao.vfo@gmail.com>
2022-11-25 09:08:04 -0400

    feat: traducao do Yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 
+import './shared/forms/TraducaoYup';
+
 import { AppRoutes } from './routes';
 import { Sidebar } from './shared/components/sidebar/Sidebar';
 import { AppThemeProvider, DrawerProvider } from './shared/contexts';

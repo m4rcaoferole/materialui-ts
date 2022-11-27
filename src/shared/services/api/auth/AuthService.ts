71167b07e38601dba48ddba85ced615049de4081
Marcos Vinicius <marcao.vfo@gmail.com>
2022-11-27 17:16:20 -0400

    feat: adicionando rota de auth - construindo service de autenticacao simples

---
+++ src/shared/services/api/auth/AuthService.ts
@@ -0,0 +1,25 @@
+import { api } from '../axi-config';
+
+interface IAuth {
+  accessToken: string;
+}
+
+const auth = async (email: string, password: string): Promise<IAuth | Error> => {
+  try {
+    const { data } = await api.get('/auth', { data: { email, password } });
+
+    if(data) {
+      return data;
+    }
+
+    return new Error('Erro no  login.');
+
+  } catch (error) {
+    console.error(error);
+    return new Error((error as { message: string }).message || 'Erro no  login.');
+  }
+};
+
+export const AuthService = {
+  auth
+};

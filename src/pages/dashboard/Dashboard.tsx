381b28fe2ab45f1b92da6c3ba9b708c524522239
Marcos Vinicius <marcao.vfo@gmail.com>
2022-11-27 17:01:14 -0400

    feat: buscanso requisito do json server

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,46 @@
+import { useEffect, useState } from 'react';
 import { Box, Card, CardContent, Grid, Typography } from '@mui/material';
+
 import { Toolbar } from '../../shared/components';
 import { LayoutBasePage } from '../../shared/layouts';
+import { CidadesService } from '../../shared/services/api/cidades/CidadesService';
+import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
 export const Dashboard = () => {
+  const [isLoadingCities, setIsLoadingCities] = useState(true);
+  const [totalCountCities, setTotalCountCities] = useState(0);
+
+  const [isLoadingPeople, setIsLoadingPeople] = useState(true);
+  const [totalCountPeople, setTotalCountPeople] = useState(0);
+
+  useEffect(() => {
+    setIsLoadingCities(true);
+    setIsLoadingPeople(true);
+    
+    CidadesService.getAll(1)
+      .then((result) => {
+        setIsLoadingCities(false);
+
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          setTotalCountCities(result.totalCount);
+        }
+      });
+    
+    PessoasService.getAll(1)
+      .then((result) => {
+        setIsLoadingPeople(false);
+
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          setTotalCountPeople(result.totalCount);
+        }
+      });
+  }, []);
+
+
   return (
     <LayoutBasePage 
       title="Página Inicial"
@@ -19,9 +57,16 @@ export const Dashboard = () => {
                   </Typography>
 
                   <Box padding={4} display='flex' justifyContent='center' alignItems='center'>
-                    <Typography variant='h1'>
-                      25
-                    </Typography>
+                    { !isLoadingCities &&(
+                      <Typography variant='h1'>
+                        {totalCountCities}
+                      </Typography>
+                    )}
+                    { isLoadingCities && (
+                      <Typography variant='h4'>
+                        Carregando...
+                      </Typography>
+                    )}
                   </Box>
 
                 </CardContent>
@@ -36,9 +81,16 @@ export const Dashboard = () => {
                   </Typography>
 
                   <Box padding={4} display='flex' justifyContent='center' alignItems='center'>
-                    <Typography variant='h1'>
-                      25
-                    </Typography>
+                    { !isLoadingPeople &&(
+                      <Typography variant='h1'>
+                        {totalCountPeople}
+                      </Typography>
+                    )}
+                    { isLoadingPeople && (
+                      <Typography variant='h4'>
+                        Carregando...
+                      </Typography>
+                    )}
                   </Box>
                 </CardContent>
               </Card>
